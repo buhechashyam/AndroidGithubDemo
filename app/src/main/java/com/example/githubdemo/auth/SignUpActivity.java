e559f9744c9cdb9e8191f02b71f7bdc94306689a
buhechashyam <shyam@moontechnolabs.com>
2024-06-03 12:54:55 +0530

    commit to auth

---
+++ app/src/main/java/com/example/githubdemo/auth/SignUpActivity.java
@@ -0,0 +1,21 @@
+package com.example.githubdemo.auth;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.githubdemo.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+    }
+}
